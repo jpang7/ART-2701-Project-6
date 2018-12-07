325a785b62a73f46448911fb9b0ae9bf7206ed71
jp2268 <jp2268@cornell.edu>
2018-12-06 23:24:16 -0500

    getting stuff to load

--- js/Boot.js
+++ js/Boot.js
@@ -1,6 +1,6 @@
 class Boot extends Phaser.State {
     preload() {
-        this.load.atlasJSONArray('spinning triangle', '../assets/spinningtri.png', '../assets/spinningtri.JSON')
+        this.load.atlasJSONArray('spinning triangle', '..assets/spinningtri.png', '..assets/spinningtri.JSON')
     }
     create() {
         console.log("Booted")
